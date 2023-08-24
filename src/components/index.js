f307f05b0fa20a0b5a856e5f613742c242708719
Lokteeva <swsw06@mail.ru>
2023-08-24 10:21:56 +0300

    реализован popup в SortPopup

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import Header from "./Header";
+import Categories from "./Categories";
+import SortPopap from "./SortPopap";
+
+export {Button, Header, Categories, SortPopap};
