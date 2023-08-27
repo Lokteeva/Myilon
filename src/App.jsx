a0f9ecacaa5cf1ca4a7795c7d3c5151e71baa961
Lokteeva <swsw06@mail.ru>
2023-08-27 15:30:07 +0300

    начинаем подключать mockapi

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import { Route, Routes } from 'react-router-dom';
 
 function App() {  
 
+  fetch('https://64eb3e98e51e1e82c57722ed.mockapi.io/pizzas').then(res => {
+    return res.json();
+  }) .then((arr)=>{
+    console.log(arr);
+  });
+
   return (
     <div className="wrapper">
       <Header /> 
